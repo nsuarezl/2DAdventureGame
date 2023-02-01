f25fe61a9507e427db702449f0f375cb91161a96
nelson <nsuarezlarre@gmail.com>
2023-01-31 23:27:38 -0500

    YOu win and you lose text and also sprites

--- mazeTest.js
+++ mazeTest.js
@@ -16,7 +16,7 @@ class Maze {
     generate() {
       for (let row = 0; row < this.rows; row++) {
         for (let col = 0; col < this.cols; col++) {
-          this.grid[row][col] = Math.random() > 0.7 ? 1 : 0;
+          this.grid[row][col] = Math.random() > 0.85 ? 1 : 0;
         }
       }
       console.log(this.grid)
@@ -59,6 +59,22 @@ class Maze {
           }
         }
       }
+
+      if(player.canMove() === false){
+        console.log("ded");
+        maze.ctx.font = "100px Arial";
+        maze.ctx.fillStyle = "red";
+        maze.ctx.fillText("YOU LOSE", 256, 512);
+        
+      }
+      if(enemy1.canMove() === false && enemy2.canMove() === false && enemy3.canMove() === false ){
+        console.log("ded");
+        maze.ctx.font = "100px Arial";
+        maze.ctx.fillStyle = "green";
+        maze.ctx.fillText("YOU WIN", 256, 512);
+        
+      }
+     
     }
     addPlayer(player) {
         this.player = player;
@@ -80,24 +96,19 @@ class Maze {
 
 
 class Entity{
-
+    
     constructor(health,maxHealth,atk,def){
         this.health = 500;
         this.maxHealth=1000;
-        this.atk=50;
-        this.def=20;
+        this.atk=1;
+        this.def=1;
+        
     }
 
-    draw() {
-        this.maze.ctx.fillStyle = this.color;
-        this.maze.ctx.fillRect(
-          this.x * this.blockSize,
-          this.y * this.blockSize,
-          this.blockSize,
-          this.blockSize
-        );
-        this.drawHealthBar()
-      }
+    h(){
+        return this.health;
+    }
+    
     canMove(){
         if (this.health>0){
             
@@ -105,6 +116,7 @@ class Entity{
             
         }
         this.color = "black";
+        console.log("was")
         return false;
         
     }
@@ -128,29 +140,54 @@ class Entity{
 }
 
 class Player extends Entity{
-    constructor(maze, x, y, color,health,maxHealth,atk,def) {
+    constructor(maze, x, y, color,imgSrc,health,maxHealth,atk,def) {
       super(health,maxHealth,atk,def);
+     
       this.maze = maze;
       this.x = x;
       this.y = y;
       this.color = color;
       this.blockSize = maze.blockSize;
       this.attack = (atk-20)*Math.floor(Math.random() * 6)+1;
+      this.imgSrc=imgSrc;
     }
 
-    checkColission(enemy){
+    checkColission(enemy,potion1,potion2){
         if(this.x === enemy.x && this.y === enemy.y)
             enemy.health-=this.attack;
+        if(this.x === potion1.x && this.y === potion1.y){
+            this.health+=potion1.reward;
+            potion.reward = 0;
+            potion1.color = "black"
+        }
+        if(this.x === potion2.x && this.y === potion2.y){
+            this.attack+=potion2.reward;
+            potion2.reward = 0;
+            potion2.color = "black"
+        }
     }
- 
+    
+    draw() {
+      let img = new Image();
+      img.src = this.imgSrc;
+      
+      this.maze.ctx.drawImage(img,this.x * this.blockSize, this.y * this.blockSize,this.blockSize,this.blockSize)
+      // this.maze.ctx.fillRect(
+      //   this.x * this.blockSize,
+      //   this.y * this.blockSize,
+      //   this.blockSize,
+      //   this.blockSize
+      // );
+      this.drawHealthBar()
+    }
     
     moveUp() {
         if(this.canMove()){
-            if (this.maze.grid[this.x][this.y-1] !== 1) {
+            if (this.maze.grid[this.x][this.y-1] !== 1 ) {
                 this.y -= 1;
-                this.checkColission(enemy1);
-                this.checkColission(enemy2);
-                this.checkColission(enemy3);
+                this.checkColission(enemy1,potion,potion1);
+                this.checkColission(enemy2,potion,potion1);
+                this.checkColission(enemy3,potion,potion1);
                     
             }
         }
@@ -158,11 +195,11 @@ class Player extends Entity{
   
     moveDown() {
         if(this.canMove()){
-            if (this.y < this.maze.rows - 1 && this.maze.grid[this.x][this.y+1] !== 1) {
+            if (this.y < this.maze.rows - 1 && this.maze.grid[this.x][this.y+1] !== 1 ) {
                 this.y += 1;
-                this.checkColission(enemy1);
-                this.checkColission(enemy2);
-                this.checkColission(enemy3);
+                this.checkColission(enemy1,potion,potion1);
+                this.checkColission(enemy2,potion,potion1);
+                this.checkColission(enemy3,potion,potion1);
             }
         }
        
@@ -172,9 +209,10 @@ class Player extends Entity{
         if(this.canMove()){
             if (this.x > 0 && this.maze.grid[this.x-1][this.y] !== 1) {
                 this.x -= 1;
-                this.checkColission(enemy1);
-                this.checkColission(enemy2);
-                this.checkColission(enemy3);
+                console.log("left");
+                this.checkColission(enemy1,potion,potion1);
+                this.checkColission(enemy2,potion,potion1);
+                this.checkColission(enemy3,potion,potion1);
             }
         }
         
@@ -184,11 +222,13 @@ class Player extends Entity{
         if(this.canMove()){
             if (this.x < this.maze.cols - 1 && this.maze.grid[this.x+1][this.y] !== 1) {
                 this.x += 1;
-                this.checkColission(enemy1);
-                this.checkColission(enemy2);
-                this.checkColission(enemy3);
+                this.checkColission(enemy1,potion,potion1);
+                this.checkColission(enemy2,potion,potion1);
+                this.checkColission(enemy3,potion,potion1);
+                
             }
         }
+        
         
     }
     
@@ -196,27 +236,44 @@ class Player extends Entity{
         
 
 class Enemy extends Entity{
-    constructor(maze, color, startX, startY,health,maxHealth,atk,def) {
+    constructor(maze, color, startX, startY,imgSrc,health,maxHealth,atk,def) {
       super(health,maxHealth,atk,def)
       this.maze = maze;
       this.color = color;
       this.blockSize = this.maze.blockSize;
       this.x = startX;
       this.y = startY;
-      this.attack = (atk-player.def)*Math.floor(Math.random() * 6)+1;
+      this.attack = (atk-player.def)*(Math.floor(Math.random() * 6)+1);
+      this.imgSrc = imgSrc;
     }
     checkColission(player){
         if(this.x === player.x && this.y === player.y)
+            
             player.health-=this.attack;
             console.log(player.health)
     }
+
+    draw() {
+      let img1 = new Image();
+      img1.src = this.imgSrc;
+      
+      this.maze.ctx.drawImage(img1,this.x * this.blockSize, this.y * this.blockSize,this.blockSize,this.blockSize)
+      // this.maze.ctx.fillRect(
+      //   this.x * this.blockSize,
+      //   this.y * this.blockSize,
+      //   this.blockSize,
+      //   this.blockSize
+      // );
+      this.drawHealthBar()
+    }
+
     move() {
       // Generate a random number to determine the direction of movement
       let move = Math.floor(Math.random() * 4);
       switch (move) {
         case 0:
           if(this.canMove()){
-            if (this.y > 0 && this.maze.grid[this.x][this.y-1] !== 1) {
+            if (this.y > 0 && this.maze.grid[this.x][this.y-1] !== 1 ) {
                 this.y -= 1;
                 this.checkColission(player);
               }
@@ -276,10 +333,17 @@ class Potions{
             this.maze.ctx.arc(this.x * 64 + 42, this.y * 64 + 45, 20, 0, 2 * Math.PI);
             this.maze.ctx.arc(this.x * 64 + 20, this.y * 64 + 45, 20, 0, 2 * Math.PI);
             this.maze.ctx.arc(this.x * 64 + 30, this.y * 64 + 35, 20, 0, 2 * Math.PI);
-            this.maze.ctx.fillStyle = "blue";
+            this.maze.ctx.fillStyle = this.color;
             this.maze.ctx.fill();
+            
     
     }
+    checkColission(){
+        if(this.maze.grid[this.x][this.y] ===1)
+            this.x+=0;
+            this.y-=0;
+    }
+
 }
 
 class healthPotion extends Potions{
@@ -290,14 +354,71 @@ class healthPotion extends Potions{
         this.blockSize = this.maze.blockSize;
         this.x = x;
         this.y = y;
+        this.checkColission();
     }
 }
-  
+
+class fightPotion extends Potions{
+    constructor(maze,reward,color,x,y){
+        super(reward);
+        this.maze = maze;
+        this.color = color;
+        this.blockSize = this.maze.blockSize;
+        this.x = x;
+        this.y = y;
+        this.checkColission();
+    }
+}
+
+
   const maze = new Maze(16, 16, "maze-canvas");
   maze.generate();
+
+var playerx=Math.floor(Math.random() *15)+1;
+var playery=Math.floor(Math.random() *15)+1;
+console.log(playerx);
+console.log(playery);
+while(maze.grid[playerx][playery] === 1){
+  playerx=Math.floor(Math.random() *15)+1;
+  playery=Math.floor(Math.random() *15)+1;
+}
+
+var enemy1x=Math.floor(Math.random() *15)+1;
+var enemy1y=Math.floor(Math.random() *15)+1;
+
+while(maze.grid[enemy1x][enemy1y] === 1){
+  enemy1x=Math.floor(Math.random() *15)+1;
+  enemy1y=Math.floor(Math.random() *15)+1;
+}
+
+
+var enemy2x=Math.floor(Math.random() *15)+1;
+var enemy2y=Math.floor(Math.random() *15)+1;
+while(maze.grid[enemy2x][enemy2y] === 1){
+  enemy2x=Math.floor(Math.random() *15)+1;
+  enemy2y=Math.floor(Math.random() *15)+1;
+}
+
+
+var enemy3x=Math.floor(Math.random() *15)+1;
+var enemy3y=Math.floor(Math.random() *15)+1;
+
+while(maze.grid[enemy3x][enemy3y] === 1){
+  enemy3x=Math.floor(Math.random() *15)+1;
+  enemy3y=Math.floor(Math.random() *15)+1;
+}
+
+
+  const player = new Player(maze, playerx, playery, "red","player.png");
+
+  const enemy1 = new Enemy(maze, "red", enemy1x, enemy1y,'monstr.png');
+  const enemy2 = new Enemy(maze, "blue", enemy2x, enemy2y,'monsterblu.png');
+  const enemy3 = new Enemy(maze, "green", enemy3x, enemy3y,'monsterpink.png');
+
+maze.addEnemy(enemy1,enemy2,enemy3)
   maze.render();
   
-  const player = new Player(maze, 1, 1, "red");
+  
 
         maze.addPlayer(player);
 
@@ -316,6 +437,7 @@ class healthPotion extends Potions{
         player.moveDown();
         break;
         }
+
         maze.render();
         player.draw();
         potion.draw();
@@ -328,12 +450,9 @@ class healthPotion extends Potions{
 
 
         
-const enemy1 = new Enemy(maze, "red", 5, 5);
-const enemy2 = new Enemy(maze, "blue", 10, 10);
-const enemy3 = new Enemy(maze, "green", 11, 11);
 
-maze.addEnemy(enemy1,enemy2,enemy3)
 setInterval(() => {
+ 
     
     enemy1.move();
     enemy2.move();
@@ -345,11 +464,25 @@ setInterval(() => {
     enemy1.draw();
     enemy2.draw();
     enemy3.draw();
-    if(player.health<0){
-        player.color = "black";
-    }
+   
+    
+    
   }, 500);
 
-const potion= new healthPotion(maze,200,"green",11,10);
-const potion1= new healthPotion(maze,200,"blue",11,10);
+  var potx=Math.floor(Math.random() *15)+1;
+  var poty=Math.floor(Math.random() *15)+1;
+  
+  while(maze.grid[potx][poty] === 1){
+    potx=Math.floor(Math.random() *15)+1;
+    poty=Math.floor(Math.random() *15)+1;
+  }
+  var pot1x=Math.floor(Math.random() *15)+1;
+  var pot1y=Math.floor(Math.random() *15)+1;
+  
+  while(maze.grid[pot1x][pot1y] === 1){
+    pot1x=Math.floor(Math.random() *15)+1;
+    pot1y=Math.floor(Math.random() *15)+1;
+  }
+const potion= new healthPotion(maze,200,"#00FF00",potx,poty);
+const potion1= new fightPotion(maze,200,"#FF4433",pot1x,pot1y);
 maze.addPotion(potion,potion1);
